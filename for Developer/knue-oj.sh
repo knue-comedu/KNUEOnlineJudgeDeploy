d883ecb36d3012fb44753b98ecb40d052404297f
itmir913 <itmir913@gmail.com>
2023-08-30 14:47:11 +0900

    knue-oj.sh

---
+++ for Developer/knue-oj.sh
@@ -0,0 +1,11 @@
+mkdir knue-oj
+cd knue-oj
+
+git clone https://github.com/knue-comedu/Judger Judger
+git clone https://github.com/knue-comedu/JudgeServer JudgeServer
+git clone https://github.com/knue-comedu/OnlineJudgeFE OnlineJudgeFE
+git clone https://github.com/knue-comedu/OnlineJudgeBE OnlineJudgeBE
+git clone https://github.com/knue-comedu/OnlineJudgeDeploy OnlineJudgeDeploy
+git clone git@github.com:knue-comedu/OJ-AI.git OJ-AI
+
+cp OnlineJudgeDeploy/docker-compose.yml docker-compose.yml
